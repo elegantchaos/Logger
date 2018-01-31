36a08ce7a37dd1ecfd21815a32810bb84bdabb78
Sam Deane <sam@elegantchaos.com>
2018-01-31 17:24:42 +0000

    Added some initial code and some basic tests.

--- Sources/Logger/Logger.swift
+++ Sources/Logger/Logger.swift
@@ -1,3 +1,64 @@
-struct Logger {
-    var text = "Hello, World!"
+
+public class LogContext : CustomStringConvertible {
+  let file: String
+  let line : Int
+  let function : String
+  let column : Int
+  
+  public init(file: String = #file, line: Int = #line,  column: Int = #column, function: String = #function) {
+    self.file = file
+    self.line = line
+    self.function = function
+    self.column = column
+  }
+  
+  public var description: String { get {
+    return "\(file): \(line),\(column) - \(function)"
+    }
+  }
 }
+
+class LogHandler {
+  let name : String
+  
+  init(_ name : String) {
+    self.name = name
+  }
+  
+  func log(channel: Logger, context : LogContext, logged : () -> Any) {
+    print("\(channel.name): \(logged())")
+  }
+}
+
+class Logger {
+  static let defaultHandler = LogHandler("default")
+  
+  let name : String
+  var handlers : [LogHandler] = []
+  let handlersSetup : () -> [LogHandler]
+  var enabled = false
+  var setup = false
+  
+  init(_ name : String, handlers : @autoclosure @escaping () -> [LogHandler] = [defaultHandler]) {
+    self.name = name
+    self.handlersSetup = handlers
+  }
+  
+  func log(_ logged : @autoclosure () -> Any, file: String = #file, line: Int = #line,  column: Int = #column, function: String = #function) {
+    if (!setup) {
+      self.handlers = self.handlersSetup()
+      self.setup = true
+    }
+    if (enabled) {
+      let context = LogContext(file:file, line:line, column:column, function:function)
+      handlers.forEach({ $0.log(channel:self, context:context, logged:logged) })
+    }
+  }
+  
+  func debug(_ logged : @autoclosure () -> Any, file: String = #file, line: Int = #line,  column: Int = #column, function: String = #function) {
+    #if DEBUG
+      log(logged, file: file, line: line, column: column, function: function)
+    #endif
+  }
+}
+
--- Tests/LoggerTests/LoggerTests.swift
+++ Tests/LoggerTests/LoggerTests.swift
@@ -1,16 +1,34 @@
 import XCTest
 @testable import Logger
 
+class TestHandler : LogHandler {
+  var logged : [Any] = []
+  
+  override func log(channel: Logger, context: LogContext, logged: () -> Any) {
+    self.logged.append(logged())
+  }
+}
+
 class LoggerTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Logger().text, "Hello, World!")
+    func testLoggingEnabled() {
+      let handler = TestHandler("test")
+      let logger = Logger("test", handlers: [handler])
+      logger.enabled = true
+      logger.log("blah")
+      XCTAssert(handler.logged.count == 1)
+      XCTAssert(handler.logged[0] as! String == "blah")
     }
 
+  func testLoggingDisabled() {
+    let handler = TestHandler("test")
+    let logger = Logger("test", handlers: [handler])
+    logger.enabled = false
+    logger.log("blah")
+    XCTAssert(handler.logged.count == 0)
+  }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testLoggingEnabled", testLoggingEnabled),
+        ("testLoggingDisabled", testLoggingDisabled),
     ]
 }
