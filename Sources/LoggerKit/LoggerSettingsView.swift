43e14fc5ed7eff89bb00de3a875057d78788352f
Sam Deane <sam@elegantchaos.com>
2019-01-30 12:40:10 +0000

    Use UITableViewController to do the heavy lifting.

--- Sources/LoggerKit/LoggerSettingsView.swift
+++ Sources/LoggerKit/LoggerSettingsView.swift
@@ -7,30 +7,11 @@
 import UIKit
 import Logger
 
-public class LoggerSettingsView: UIViewController, UITableViewDelegate, UITableViewDataSource {
+public class LoggerSettingsView: UITableViewController {
     public typealias DoneCallback = () -> Void
     
-    var tableView: UITableView!
     let manager = Logger.defaultManager
-
-    public override func loadView() {
-        super.loadView()
-        
-        tableView = UITableView()
-        tableView.allowsSelection = true
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.sectionHeaderHeight = 40
-        view.addSubview(tableView)
-        
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
-        tableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
-        tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
-        tableView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
-        
-        tableView.setNeedsDisplay()
-    }
+    let font = UIFont.systemFont(ofSize: UIFont.labelFontSize)
     
     public func show(in controller: UIViewController, sender: UIView, done: DoneCallback? = nil) {
         title = "Log Settings"
@@ -55,88 +36,96 @@ public class LoggerSettingsView: UIViewController, UITableViewDelegate, UITableV
         }
     }
     
-    enum Command {
-        case enableAllChannels
-        case disableAllChannels
-        case resetAllSettings
+    enum Command: String {
+        case enableAllChannels = "Enable All"
+        case disableAllChannels = "Disable All"
+        case resetAllSettings = "Reset All"
+
+        static let commands: [Command] = [
+            .enableAllChannels,
+            .disableAllChannels,
+            .resetAllSettings
+        ]
     }
     
-    struct Item {
-        let name: String
-        let accessory: UITableViewCell.AccessoryType
-        let command: Command
+    let sections = [ "Settings", "Channels" ]
+    let commandsSection = 0
+    
+    override public func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
     }
     
-    let items = [
-        Item(name: "Enable All", accessory: .disclosureIndicator, command: .enableAllChannels),
-        Item(name: "Disable All", accessory: .disclosureIndicator, command: .disableAllChannels),
-        Item(name: "Reset All", accessory: .disclosureIndicator, command: .resetAllSettings)
-    ]
-    
-//    func pushViewController(controller: UIViewController) {
-//        if let navigation = self.navController ?? self.navigationController {
-//            navigation.pushViewController(controller, animated: true)
-//        }
-//    }
-    
-    public func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+    override public func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section]
     }
     
-    public func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return section == 0 ? "Settings" : "Channels"
-    }
-    
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if section == 0 {
-            return items.count
+    override public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == commandsSection {
+            return Command.commands.count
         } else {
             return manager.registeredChannels.count
         }
     }
-    
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+   
+    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "DebugViewCell") else {
             return UITableViewCell(style: .default, reuseIdentifier: "DebugViewCell")
         }
         
-        if indexPath.section == 0 {
-            let item = items[indexPath.row]
-            cell.textLabel?.text = item.name
-            //        cell.textLabel?.font = settingsFont
-            cell.accessoryType = item.accessory
+        if indexPath.section == commandsSection {
+            setupCommandRow(indexPath, cell)
         } else {
-            let channel = manager.registeredChannels[indexPath.row]
-            cell.textLabel?.text = channel.name
-            cell.accessoryType = channel.enabled ? .checkmark : .none
+            setupChannelRow(indexPath, cell)
         }
+
+        cell.textLabel?.font = font
         return cell
     }
     
-    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if indexPath.section == 0 {
-            let item = items[indexPath.row]
-            switch item.command {
-            case .enableAllChannels:
-                manager.update(channels: manager.registeredChannels, state: true)
-                tableView.reloadSections(IndexSet([1]), with: .automatic)
-                
-            case .disableAllChannels:
-                manager.update(channels: manager.registeredChannels, state: false)
-                tableView.reloadSections(IndexSet([1]), with: .automatic)
-
-            case .resetAllSettings:
-                break
-            }
+    override public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == commandsSection {
+            selectCommand(indexPath, tableView)
         } else {
-            let channel = manager.registeredChannels[indexPath.row]
-            manager.update(channels: [channel], state: !channel.enabled)
-            tableView.reloadRows(at: [indexPath], with: .automatic)
-
+            selectChannel(indexPath, tableView)
         }
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
+
     
+    fileprivate func setupCommandRow(_ indexPath: IndexPath, _ cell: UITableViewCell) {
+        let command = Command.commands[indexPath.row]
+        cell.textLabel?.text = command.rawValue
+        cell.accessoryType = .none
+    }
+    
+    fileprivate func setupChannelRow(_ indexPath: IndexPath, _ cell: UITableViewCell) {
+        let channel = manager.registeredChannels[indexPath.row]
+        cell.textLabel?.text = channel.name
+        cell.accessoryType = channel.enabled ? .checkmark : .none
+    }
+    
+    fileprivate func selectCommand(_ indexPath: IndexPath, _ tableView: UITableView) {
+        let item = Command.commands[indexPath.row]
+        switch item {
+        case .enableAllChannels:
+            manager.update(channels: manager.registeredChannels, state: true)
+            tableView.reloadSections(IndexSet([1]), with: .automatic)
+            
+        case .disableAllChannels:
+            manager.update(channels: manager.registeredChannels, state: false)
+            tableView.reloadSections(IndexSet([1]), with: .automatic)
+            
+        case .resetAllSettings:
+            break
+        }
+    }
+    
+    fileprivate func selectChannel(_ indexPath: IndexPath, _ tableView: UITableView) {
+        let channel = manager.registeredChannels[indexPath.row]
+        manager.update(channels: [channel], state: !channel.enabled)
+        tableView.reloadRows(at: [indexPath], with: .automatic)
+    }
+
 }
 #endif
