b11580834c9414ff066c7e1ab0d886f897d3ef20
Sam Deane <sam@elegantchaos.com>
2019-06-17 15:55:46 +0100

    tidied package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version:5.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -9,7 +8,6 @@ let package = Package(
         .macOS(.v10_13), .iOS(.v12),
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Logger",
             targets: ["Logger"]),
@@ -25,12 +23,8 @@ let package = Package(
         )
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Logger",
             dependencies: []),
@@ -49,4 +43,3 @@ let package = Package(
     ],
     swiftLanguageVersions: [.v4_2]
   )
-
