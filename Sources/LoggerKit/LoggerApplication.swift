511df6a4f1ea92cf9a04d3035e1a8aa6522d5920
Sam Deane <sam@elegantchaos.com>
2020-02-04 16:35:36 +0000

    Added standard UIKit app and scene delegates.

---
+++ Sources/LoggerKit/LoggerApplication.swift
@@ -0,0 +1,76 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//  Created by Sam Deane on 04/02/20.
+//  All code (c) 2020 - present day, Elegant Chaos Limited.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+#if canImport(UIKit)
+import UIKit
+import Logger
+
+let applicationChannel = Channel("Application", handlers: [OSLogHandler()])
+
+/// Standardise AppDelegate which:
+/// - logs the main application events
+/// - installs a Debug/Logger menu in the responder chain
+open class LoggerApplication: UIResponder {
+    let loggerMenu = LoggerMenu(manager: Logger.defaultManager)
+    public var window: UIWindow?
+    
+    open override var next: UIResponder? {
+        return loggerMenu
+    }
+}
+
+extension LoggerApplication: UIApplicationDelegate {
+    open func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        applicationChannel.enabled = true
+        applicationChannel.debug("didFinishLaunching")
+        if let options = launchOptions {
+            applicationChannel.debug("launch options: \(options)")
+        }
+
+        return true
+    }
+
+    open func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        applicationChannel.debug("willResignActive")
+    }
+
+    open func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        applicationChannel.debug("didEnterBackground")
+    }
+
+    open func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        applicationChannel.debug("willEnterForeground")
+    }
+
+    open func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        applicationChannel.debug("didBecomeActive")
+    }
+
+    open func application(_ app: UIApplication, open inputURL: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        applicationChannel.debug("open \(inputURL)")
+        return false
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    open func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    open func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+}
+#endif
---
+++ Sources/LoggerKit/LoggerScene.swift
@@ -0,0 +1,60 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//  Created by Sam Deane on 04/02/20.
+//  All code (c) 2020 - present day, Elegant Chaos Limited.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+#if canImport(UIKit)
+import UIKit
+import Logger
+
+let sceneChannel = Channel("Scene", handlers: [OSLogHandler()])
+
+/// Standardised scene delegate which logs its main events.
+open class LoggerScene: UIResponder {
+    public var window: UIWindow?
+}
+
+extension LoggerScene: UIWindowSceneDelegate {
+    public func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        sceneChannel.debug("willConnectTo")
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+    public func sceneDidDisconnect(_ scene: UIScene) {
+        // Called as the scene is being released by the system.
+        // This occurs shortly after the scene enters the background, or when its session is discarded.
+        // Release any resources associated with this scene that can be re-created the next time the scene connects.
+        // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
+        sceneChannel.debug("sceneDidDisconnect")
+    }
+
+    public func sceneDidBecomeActive(_ scene: UIScene) {
+        // Called when the scene has moved from an inactive state to an active state.
+        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        sceneChannel.debug("sceneDidBecomeActive")
+    }
+
+    public func sceneWillResignActive(_ scene: UIScene) {
+        // Called when the scene will move from an active state to an inactive state.
+        // This may occur due to temporary interruptions (ex. an incoming phone call).
+        sceneChannel.debug("sceneWillResignActive")
+    }
+
+    public func sceneWillEnterForeground(_ scene: UIScene) {
+        // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the changes made on entering the background.
+        sceneChannel.debug("sceneWillEnterForeground")
+    }
+
+    public func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+        sceneChannel.debug("sceneDidEnterBackground")
+    }
+
+}
+#endif
