87a8213f791bf68b5bbe4430e0d5cb575facbe51
Sam Deane <sam@elegantchaos.com>
2020-02-19 10:33:51 +0000

    Added missing handlers. Don't default the channel to on.

--- Sources/LoggerKit/LoggerApplication.swift
+++ Sources/LoggerKit/LoggerApplication.swift
@@ -26,7 +26,6 @@ let applicationChannel = Channel("Application", handlers: [OSLogHandler()])
 @available(iOS 13.0, *) extension LoggerApplication: UIApplicationDelegate {
     open func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        applicationChannel.enabled = true
         applicationChannel.debug("didFinishLaunching")
         if let options = launchOptions {
             applicationChannel.debug("launch options: \(options)")
@@ -69,15 +68,12 @@ let applicationChannel = Channel("Application", handlers: [OSLogHandler()])
 @available(iOS 13.0, *) extension LoggerApplication {
 
     open func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
+        applicationChannel.debug("configurationForConnecting \(connectingSceneSession) \(options)")
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     open func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+        applicationChannel.debug("didDiscardSceneSessions \(sceneSessions)")
     }
 }
 
