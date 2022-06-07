2676890c843d95a91fcc510ba6736d4f250f6068
Sam Deane <sam@elegantchaos.com>
2022-06-07 12:26:38 +0100

    Added formatter tool

--- Package.swift
+++ Package.swift
@@ -1,12 +1,14 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.6
 
 import PackageDescription
 
 let package = Package(
     name: "Logger",
+    
     platforms: [
         .macOS(.v10_13), .iOS(.v12), .tvOS(.v12), .watchOS(.v5)
     ],
+    
     products: [
         .library(
             name: "Logger",
@@ -19,41 +21,59 @@ let package = Package(
             targets: ["LoggerKit"]),
         .library(
             name: "LoggerTestSupport",
-            targets: ["LoggerTestSupport"])
+            targets: ["LoggerTestSupport"]),
+        .plugin(
+            name: "Formatter",
+            targets: ["FormatterPlugin"]
+        ),
     ],
     
     dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.41.2")
     ],
-
+    
     targets: [
         .target(
             name: "Logger",
             dependencies: []),
-        .target(
-            name: "LoggerKit",
-            dependencies: ["Logger"]),
-        .target(
-            name: "LoggerUI",
-            dependencies: ["Logger"]),
-        .target(
-            name: "LoggerTestSupport",
-            dependencies: ["Logger"]),
-        .testTarget(
-            name: "LoggerTests",
-            dependencies: ["Logger", "LoggerTestSupport"]),
-        .testTarget(
-            name: "LoggerKitTests",
-            dependencies: ["LoggerKit", "LoggerTestSupport"])
+        
+            .target(
+                name: "LoggerKit",
+                dependencies: ["Logger"]
+            ),
+        
+            .target(
+                name: "LoggerUI",
+                dependencies: ["Logger"]
+            ),
+        
+            .target(
+                name: "LoggerTestSupport",
+                dependencies: ["Logger"]
+            ),
+        
+            .testTarget(
+                name: "LoggerTests",
+                dependencies: ["Logger", "LoggerTestSupport"]
+            ),
+        
+            .testTarget(
+                name: "LoggerKitTests",
+                dependencies: ["LoggerKit", "LoggerTestSupport"]
+            ),
+        
+            .plugin(name: "FormatterPlugin",
+                    capability: .command(
+                        intent: .sourceCodeFormatting(),
+                        permissions: [
+                            .writeToPackageDirectory(reason: "This command reformats source files")
+                        ]
+                    ),
+                    dependencies: [
+                        .product(name: "swiftformat", package: "SwiftFormat"),
+                    ]
+                   )
+        
     ]
-  )
-
-#if swift(>=5.6)
-  // Add the documentation compiler plugin if possible
-  package.dependencies.append(
-    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
-  )
-
-package.dependencies.append(
-    .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.41.2")
-  )
-#endif
+)
---
+++ Plugins/FormatterPlugin/FormatterPlugin.swift
@@ -0,0 +1,40 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//  Created by Sam Deane on 07/06/22.
+//  All code (c) 2022 - present day, Elegant Chaos Limited.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+import Foundation
+import PackagePlugin
+
+@main
+struct FormatterPlugin: CommandPlugin {
+    
+    func performCommand(context: PluginContext, arguments: [String]) throws {
+        let swiftFormatTool = try context.tool(named: "swiftformat")
+        let swiftFormatExec = URL(fileURLWithPath: swiftFormatTool.path.string)
+//        let configFile = context.package.directory.appending(".swift-format.json")
+        
+        for target in context.package.targets {
+            guard let target = target as? SourceModuleTarget else { continue }
+
+            let process = Process()
+            process.executableURL = swiftFormatExec
+            process.arguments = [
+//                "--configuration", "\(configFile)",
+                "--in-place",
+                "--recursive",
+                "\(target.directory)",
+            ]
+            try process.run()
+            process.waitUntilExit()
+
+            if process.terminationReason == .exit && process.terminationStatus == 0 {
+                print("Formatted the source code in \(target.directory).")
+            }
+            else {
+                let problem = "\(process.terminationReason):\(process.terminationStatus)"
+                Diagnostics.error("swift-format invocation failed: \(problem)")
+            }
+        }
+    }
+}
