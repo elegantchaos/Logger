5d4bb4fcc961807f1409c712da64ee4c385308ae
Sam Deane <sam@elegantchaos.com>
2022-07-11 16:37:46 +0100

    Bumped plugin version

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
     ],
     
     dependencies: [
-        .package(url: "https://github.com/elegantchaos/SwiftFormatterPlugin.git", from: "0.0.1"),
+        .package(url: "https://github.com/elegantchaos/SwiftFormatterPlugin.git", from: "1.0.0"),
     ],
     
     targets: [
