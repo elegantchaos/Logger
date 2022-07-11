da121d7962cca5572271e4bb9874c7baa151759c
Sam Deane <sam@elegantchaos.com>
2022-07-11 15:36:55 +0100

    Added formatter plugin

--- Package.swift
+++ Package.swift
@@ -1,12 +1,14 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.6
 
 import PackageDescription
 
 let package = Package(
     name: "Logger",
+    
     platforms: [
         .macOS(.v10_13), .iOS(.v12), .tvOS(.v12), .watchOS(.v5)
     ],
+    
     products: [
         .library(
             name: "Logger",
@@ -21,7 +23,11 @@ let package = Package(
             name: "LoggerTestSupport",
             targets: ["LoggerTestSupport"])
     ],
-    dependencies: [],
+    
+    dependencies: [
+        .package(url: "https://github.com/elegantchaos/SwiftFormatterPlugin.git", from: "0.0.1"),
+    ],
+    
     targets: [
         .target(
             name: "Logger",
