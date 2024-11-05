c6d619809e9a658331a70514340f4246f5f05134
Sam Deane <sam@elegantchaos.com>
2024-11-05 16:39:20 +0000

    bumped minimum iOS to 13

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "Logger",
     
     platforms: [
-        .macOS(.v10_13), .iOS(.v12), .tvOS(.v12), .watchOS(.v5)
+        .macOS(.v10_13), .iOS(.v13), .tvOS(.v13), .watchOS(.v5)
     ],
     
     products: [
