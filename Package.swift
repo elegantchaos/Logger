8117edd39b11d4c8ee0649679b480a73eaf15c3d
Sam Deane <sam@elegantchaos.com>
2025-08-28 15:36:03 +0100

    removed formatter plugin

--- Package.swift
+++ Package.swift
@@ -3,23 +3,37 @@
 import Foundation
 import PackageDescription
 
+let usePlugins = ProcessInfo.processInfo.environment["RESOLVE_COMMAND_PLUGINS"] != nil
+
 let package = Package(
   name: "Logger",
 
   platforms: [
-    .macOS(.v15), .macCatalyst(.v18), .iOS(.v18), .tvOS(.v18), .watchOS(.v11),
+    .macOS(.v15),
+    .macCatalyst(.v18),
+    .iOS(.v18),
+    .tvOS(.v18),
+    .watchOS(.v11),
+    .custom(
+      "Ubuntu", versionString: "22.04"
+    ),
   ],
 
   products: [
     .library(
       name: "Logger",
-      targets: ["Logger"]),
+      targets: ["Logger"]
+    ),
+
     .library(
       name: "LoggerUI",
-      targets: ["LoggerUI"]),
+      targets: ["LoggerUI"]
+    ),
+
     .library(
       name: "LoggerTestSupport",
-      targets: ["LoggerTestSupport"]),
+      targets: ["LoggerTestSupport"]
+    ),
   ],
 
   dependencies: [],
@@ -27,22 +41,42 @@ let package = Package(
   targets: [
     .target(
       name: "Logger",
-      dependencies: []),
+      dependencies: [],
+      plugins: usePlugins
+        ? [
+          .plugin(name: "ActionBuilderPlugin", package: "ActionBuilderPlugin")
+        ] : []
+    ),
+
     .target(
       name: "LoggerUI",
-      dependencies: ["Logger"]),
+      dependencies: [
+        "Logger"
+      ],
+    ),
+
     .target(
       name: "LoggerTestSupport",
-      dependencies: ["Logger"]),
+      dependencies: [
+        "Logger"
+      ]
+    ),
+
     .testTarget(
       name: "LoggerTests",
-      dependencies: ["Logger", "LoggerTestSupport"]),
+      dependencies: [
+        "Logger",
+        "LoggerTestSupport",
+      ]
+    ),
   ]
 )
 
-if ProcessInfo.processInfo.environment["RESOLVE_COMMAND_PLUGINS"] != nil {
+if usePlugins {
   package.dependencies.append(contentsOf: [
-    .package(url: "https://github.com/elegantchaos/ActionBuilderPlugin.git", from: "1.0.8"),
-    .package(url: "https://github.com/elegantchaos/SwiftFormatterPlugin.git", from: "1.0.3"),
+    .package(
+      url: "https://github.com/elegantchaos/ActionBuilderPlugin.git",
+      from: "2.0.1"
+    )
   ])
 }
